541c8cecef59eb86b165ae58e335bd22d61f795c
MD.SUJAN AHMED <csesujon155@gmail.com>
2023-03-08 18:29:18 +0600

    simple

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- ABOUT START -->
-  <div class="about-area pt-80 pb-50" id="about">
+  <div class="about-area pt-80 pb-50 grey-bg" id="about">
 			<div class="container">
 				<div class="habout">
 					<div class="row">
@@ -9,7 +9,7 @@
 								<h3 class="animate__animated animate__fadeInUp animate__delay-.5s">Company at a glance</h3>
 								<p class="animate__animated animate__fadeInUp animate__delay-.8s">Moon Star Publicity starts journey in the real estate business in 2010 with a view to providing esthetic apartments for the contemporary city lifestyle. For more than 12 years, we have grown ourselves as a developer for providing functional apartments to our deserving clients.</p>
 								<p>Inspired by the unique architectural thought our apartments arranged in a truly unique cascade of nature for luxury living.</p>
-								<div class="row animate__animated animate__fadeInUp animate__delay-1s">
+								<div class="row animate__animated animate__fadeInUp animate__delay-.5s">
 									<div class="col-xl-6 col-lg-6 col-md-6">
 										<ul>
 											<li><font-awesome-icon icon="fa-solid fa-thumbs-up"/> Awesome Design</li>
@@ -33,7 +33,7 @@
 							</div>
 						</div>
 						<div class="col-xl-6 col-lg-6 col-md-12">
-							<div class="habout__img mb-30 animate__animated animate__fadeInUp animate__delay-1s">
+							<div class="habout__img mb-30 animate__animated animate__fadeInRight animate__delay-1s">
 								<img src="../assets/images/about/about.jpg" alt="" class="img-fluid rounded img-thumbnail">
 							</div>
 						</div>
@@ -51,7 +51,7 @@
 							</div>
 						</div>
 						<div class="col-sm-6 col-md-4">
-							<div class="ability__single animate__animated animate__slideInUp animate__delay-1.8s mb-30">
+							<div class="ability__single animate__animated animate__slideInUp animate__delay-1.6s mb-30">
 								<h4>Vision</h4>
 								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit repellendus eum quis! Quidem, iure!</p>
 								<div class="ability__single-btn">
@@ -60,7 +60,7 @@
 							</div>
 						</div>
 						<div class="col-sm-6 col-md-4">
-							<div class="ability__single animate__animated animate__slideInRight animate__delay-2s mb-30">
+							<div class="ability__single animate__animated animate__slideInRight animate__delay-1.6s mb-30">
 								<h4>History</h4>
 								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit repellendus eum quis! Quidem, iure!</p>
 								<div class="ability__single-btn">
@@ -94,6 +94,9 @@
 }
 .about-area .habout__img {
   position: relative;
+}
+.about-area .habout__content .fa-thumbs-up{
+	color: var(--moon-secondary);
 }
 
 .about-area .ability__single-btn a {
